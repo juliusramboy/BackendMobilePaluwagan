4e4c730c70d098530c6c17e4b146309e2ad4abe2
juliusramboy <juliusramboy@gmail.com>
2025-12-12 11:27:41 +0800

    security_spring

---
+++ src/main/java/com/example/MobilePaluwagan/Security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.MobilePaluwagan.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(customizer -> withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){}
+}
+
+
