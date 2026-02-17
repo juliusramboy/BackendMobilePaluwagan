fda16144b5e8e7b17790221bf6d467b183963bd8
juliusramboy <juliusramboy@gmail.com>
2026-02-17 15:51:58 +0800

    switch localDate to localDateTime

--- src/main/java/com/example/MobilePaluwagan/DTOs/Request/UserDepositSavingsRequest.java
+++ src/main/java/com/example/MobilePaluwagan/DTOs/Request/UserDepositSavingsRequest.java
@@ -3,13 +3,16 @@ package com.example.MobilePaluwagan.DTOs.Request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDepositSavingsRequest {
     private double amountDeposit;
     private LocalDate depositDate;
--- src/main/java/com/example/MobilePaluwagan/Entity/UserSavings.java
+++ src/main/java/com/example/MobilePaluwagan/Entity/UserSavings.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -31,7 +32,7 @@ public class UserSavings {
     private Status status = Status.PENDING;
 
     @Column(name = "deposit_date")
-    private LocalDate depositDate;
+    private LocalDateTime depositDate;
 
     @Column(name = "reference")
     private String reference;
