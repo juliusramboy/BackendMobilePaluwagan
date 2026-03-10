aa1fb800d4f21c4764e83876a2310ad4765f258f
juliusramboy <juliusramboy@gmail.com>
2026-03-10 16:34:36 +0800

    add pagination for notif and clear all user and admin

--- src/main/java/com/example/MobilePaluwagan/Repository/NotificationRepository.java
+++ src/main/java/com/example/MobilePaluwagan/Repository/NotificationRepository.java
@@ -1,6 +1,9 @@
 package com.example.MobilePaluwagan.Repository;
 
 import com.example.MobilePaluwagan.Entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +12,8 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    List<Notification> findByUserIdAndIsAdminFalseOrderByCreatedAtDesc(Long userId);
-    List<Notification> findByIsAdminTrueOrderByCreatedAtDesc();
+    Page<Notification> findByUserIdAndIsAdminFalseOrderByCreatedAtDesc(Long userId, Pageable pageable);
+    Page<Notification> findByIsAdminTrueOrderByCreatedAtDesc(Pageable pageable);
 
     Long countByUserIdAndIsReadFalseAndIsAdminFalse(Long userId);
     Long countByIsAdminTrue();
