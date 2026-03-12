ec3417ff61c63a9877f273261af4ec8e808154a9
juliusramboy <juliusramboy@gmail.com>
2026-03-12 20:00:17 +0800

    added pagination in payment

--- src/main/java/com/example/MobilePaluwagan/DTOs/Request/PaymentAdminRequest.java
+++ src/main/java/com/example/MobilePaluwagan/DTOs/Request/PaymentAdminRequest.java
@@ -2,7 +2,6 @@ package com.example.MobilePaluwagan.DTOs.Request;
 
 
 import com.example.MobilePaluwagan.Entity.PaymentMethod;
-import com.example.MobilePaluwagan.Entity.Status;
 import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,15 +9,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PaymentLoanRequest {
+public class PaymentAdminRequest {
 
-    private Long applicationId;
+    private String applicationId;
     private BigDecimal amount;
     private String reference;
     private PaymentMethod paymentMethod;
