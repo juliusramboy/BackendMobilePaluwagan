978a2dad81bdaa383ef83608f0441f70a171a9db
juliusramboy <juliusramboy@gmail.com>
2026-03-12 20:03:15 +0800

    no message

--- src/main/java/com/example/MobilePaluwagan/Controller/AdminPaymentController.java
+++ src/main/java/com/example/MobilePaluwagan/Controller/AdminPaymentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("api/admin/payment/")
+@RequestMapping("api/admin/payment")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminPaymentController {
 
