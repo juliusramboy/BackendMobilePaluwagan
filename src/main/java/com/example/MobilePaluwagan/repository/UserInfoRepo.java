262f6a53d7d38c583c46c82f46a18e87974c5bbd
juliusramboy <juliusramboy@gmail.com>
2026-03-18 00:24:13 +0800

    no message

--- src/main/java/com/example/MobilePaluwagan/repository/UserInfoRepo.java
+++ src/main/java/com/example/MobilePaluwagan/repository/UserInfoRepo.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public interface UserInfoRepo extends JpaRepository<UserInfo, Long> {
     Optional<UserInfo> findByUserId(Long userId);
 
-    @Query(value = "SELECT ul.id, CONCAT(up.first_name, ' ', up.last_name) AS full_name, r.role_name, up.verified_date, ul.has_savings_account, ul.has_loan " +
+    @Query(value = "SELECT ul.id, up.first_name, up.last_name, r.role_name, up.verified_date, ul.has_savings_account, ul.has_loan " +
             "FROM user_login ul " +
             "JOIN user_profile up ON ul.id = up.user_id " +
             "JOIN roles r ON r.id = ul.role_id " +
