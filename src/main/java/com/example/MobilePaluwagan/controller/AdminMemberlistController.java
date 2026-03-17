b982be8ccdd67694c23c3a23c49d02abed8ca356
juliusramboy <juliusramboy@gmail.com>
2026-03-17 18:16:37 +0800

    no message

--- src/main/java/com/example/MobilePaluwagan/controller/AdminMemberlistController.java
+++ src/main/java/com/example/MobilePaluwagan/controller/AdminMemberlistController.java
@@ -1,18 +1,18 @@
 package com.example.MobilePaluwagan.controller;
 
+import com.example.MobilePaluwagan.dto.Request.RegisterRequest;
 import com.example.MobilePaluwagan.dto.Response.MembersFilterResponse;
+import com.example.MobilePaluwagan.entity.User;
 import com.example.MobilePaluwagan.entity.UserInfo;
 import com.example.MobilePaluwagan.repository.UserInfoRepo;
 import com.example.MobilePaluwagan.service.MembersService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/memberlist")
@@ -42,5 +42,10 @@ public class AdminMemberlistController {
                 .build();
 
         return ResponseEntity.ok(membersService.filterMembers(filter, page, size));
+    }
+
+    @PostMapping("/admin")
+    public ResponseEntity<?> addAdmin(@Valid @RequestBody RegisterRequest request){
+        return membersService.adminRegister(request);
     }
 }
--- src/main/java/com/example/MobilePaluwagan/service/MembersService.java
+++ src/main/java/com/example/MobilePaluwagan/service/MembersService.java
@@ -1,14 +1,20 @@
 package com.example.MobilePaluwagan.service;
 
+import com.example.MobilePaluwagan.dto.Request.RegisterRequest;
 import com.example.MobilePaluwagan.dto.Response.MembersFilterProjection;
 import com.example.MobilePaluwagan.dto.Response.MembersFilterResponse;
-import com.example.MobilePaluwagan.entity.UserInfo;
-import com.example.MobilePaluwagan.repository.UserInfoRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.MobilePaluwagan.entity.*;
+import com.example.MobilePaluwagan.repository.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
 
 @Service
 public class MembersService {
@@ -16,9 +22,27 @@ public class MembersService {
 
     private final UserInfoRepo userInfoRepo;
 
-    public MembersService(UserInfoRepo userInfoRepo) {
+    private final RoleRepo roleRepo;
+
+    private final UserRepo userRepo;
+
+    private final UserBankRepo userBankRepo;
+
+    private final VerificationRepo verificationRepo;
+
+    private final RegisterService registerService;
+
+    public MembersService(UserInfoRepo userInfoRepo,  RoleRepo roleRepo, UserRepo userRepo, UserBankRepo userBankRepo,  VerificationRepo verificationRepo,  RegisterService registerService) {
+
         this.userInfoRepo = userInfoRepo;
+        this.roleRepo = roleRepo;
+        this.userRepo = userRepo;
+        this.userBankRepo = userBankRepo;
+        this.verificationRepo = verificationRepo;
+        this.registerService = registerService;
     }
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public Page<MembersFilterProjection> filterMembers(MembersFilterResponse filter, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
@@ -32,5 +56,44 @@ public class MembersService {
                 ? filter.getRole() : null;
 
         return userInfoRepo.filterMembers(name, surname, role, pageable);
+    }
+
+
+    public ResponseEntity<Long> adminRegister(RegisterRequest register) {
+
+        Role defaultRole = roleRepo.findById(1)
+                .orElseThrow(() -> new RuntimeException("Admin role not found"));
+
+
+        String rawPassword = register.getPassword();
+        String hashedPassword = encoder.encode(rawPassword);
+        User user = new User();
+        user.setEmail(register.getEmail());
+        user.setPassword(hashedPassword);
+        user.setRole(defaultRole);
+
+        User userdataWithId = userRepo.save(user);
+
+
+        UserVerification userVerification = new UserVerification();
+        userVerification.setUserId(userdataWithId.getId());
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(userdataWithId.getId());
+        userInfo.setFirstName(register.getFirstName());
+        userInfo.setMiddleName(register.getMiddleName());
+        userInfo.setLastName(register.getLastName());
+        userInfo.setSuffix(register.getSuffix());
+        userInfo.setPhoneNumber(register.getPhoneNumber());
+
+        userInfoRepo.save(userInfo);
+
+        UserBank userBank = new UserBank();
+        userBank.setUserId(userdataWithId.getId());
+        userBank.setAccountBalance(BigDecimal.valueOf(0L));
+
+        userBankRepo.save(userBank);
+
+        return new ResponseEntity<>(userdataWithId.getId(), HttpStatus.OK);
     }
 }
