1dfe39635fa56cf92a502da36c027c8aed32f080
juliusramboy <juliusramboy@gmail.com>
2026-03-20 23:04:23 +0800

    fix

--- src/main/java/com/example/MobilePaluwagan/service/ProfileService.java
+++ src/main/java/com/example/MobilePaluwagan/service/ProfileService.java
@@ -41,13 +41,21 @@ public class ProfileService {
     public UserProfileResponse userAllInfo(Long userId) {
         UserInfo userInfo = userInfoRepo.findByUserId(userId).orElseThrow(() -> new RuntimeException("User info not found"));
         User user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        UserBank userBank = userBankRepo.findByUserId(userId).orElseThrow(() -> new RuntimeException("User bank not found"));
+        Optional<UserBank> userBank = userBankRepo.findByUserId(userId);
         Optional<Loan> payment = userLoanRepo.findByUserId(userId);
 
-        boolean matured = !userBank.getFirstDepositDate()
-                .plusYears(1)
-                .toLocalDate()
-                .isAfter(LocalDate.now());
+        boolean isMatured = false;
+
+        if (userBank.isPresent()){
+            UserBank bank = userBank.get();
+
+            isMatured = !bank.getFirstDepositDate()
+                    .plusYears(1)
+                    .toLocalDate()
+                    .isAfter(LocalDate.now());
+        }
+
+
         BigDecimal loanBalance = BigDecimal.ZERO;
         LocalDate loanDueDate = null;
 
